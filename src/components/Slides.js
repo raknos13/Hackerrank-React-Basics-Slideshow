33d1d1326922700eba85f63f34e24fd1a6215022
Mukesh Kumar <mkso13189@gmail.com>
2024-09-10 15:33:34 +0530

    Add functionality for next, prev, restart button

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Slides({slides}) {
+function Slides({ slides }) {
+	const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
+	function handleNext() {
+		currentSlideIndex < slides.length - 1
+			? setCurrentSlideIndex(currentSlideIndex + 1)
+			: setCurrentSlideIndex(0);
+	}
 
+	function handlePrevious() {
+		currentSlideIndex > 0
+			? setCurrentSlideIndex(currentSlideIndex - 1)
+			: setCurrentSlideIndex(slides.length - 1);
+	}
+
+	function handleRestart() {
+		currentSlideIndex !== 0 && setCurrentSlideIndex(0);
+	}
+
+	return (
+		<div>
+			<div id='navigation' className='text-center'>
+				<button
+					data-testid='button-restart'
+					className='small outlined'
+					onClick={handleRestart}
+					disabled={currentSlideIndex === 0}
+				>
+					Restart
+				</button>
+				<button
+					data-testid='button-prev'
+					className='small'
+					onClick={handlePrevious}
+				>
+					Prev
+				</button>
+				<button
+					data-testid='button-next'
+					className='small'
+					onClick={handleNext}
+				>
+					Next
+				</button>
+			</div>
+			<div id='slide' className='card text-center'>
+				<h1 data-testid='title'>{slides[currentSlideIndex].title}</h1>
+				<p data-testid='text'>{slides[currentSlideIndex].text}</p>
+			</div>
+		</div>
+	);
 }
 
 export default Slides;
